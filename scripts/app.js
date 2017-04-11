3b0673a0f6ec04997c0b1cd9e7122f2e8020a802
ADL175 <armydavidlim@gmail.com>
2017-04-11 15:53:11 -0700

    added some filler js object constructor

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function MyProject('name', 'date', 'url') {
+  this.name = name;
+  this.date = date;
+  this.url = url;
+
+}
+
+var codeFellowsProject = new MyProject('Salmon Cookies', 'date', 'url');
+
+// to be filled later
