725085edf215f806fa3f6b7b3ab0247269cce9a8
ADL175 <armydavidlim@gmail.com>
2017-04-12 12:14:58 -0700

    added some template JS to add projects

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,78 @@
 'use strict';
 
-function MyProject('name', 'date', 'url') {
-  this.name = name;
-  this.date = date;
-  this.url = url;
+var rawData = [
+  {
+    title:  'Salmon Cookies',
+    author: 'David Lim',
+    publishedOn:   '2015-11-05',
+    authorUrl:  'http://www.google.com',
+    body:   '<img src="http://www.placehold.it/200>"'
+    category: 'Code Fellows 201 Project'
 
+  },
+  {
+    title:  'Bus Mall',
+    author: 'David Lim',
+    date:   '2017-11-05',
+    authorUrl:  'http://www.google.com',
+    body:   '<img src="http://www.placehold.it/200>"'
+    category: 'Code Fellows 201 Project'
+
+
+  },
+  {
+    title:  'Portfolio',
+    author: 'David Lim',
+    date:   '2016-11-05',
+    authorUrl:  'http://www.google.com',
+    body:   '<img src="http://www.placehold.it/200>"'
+    category: 'Code Fellows 201 Project'
+
+
+  },
+
+];
+
+
+// TEMPLATE EXAMPLE:
+
+var articles = [];
+
+function Article (rawDataObj) {
+
+  this.title = rawDataObj.title;
+  this.author = rawDataObj.author;
+  this.publishedOn = rawDataObj.publishedOn;
+  this.category = rawDataObj.category;
+  this.authorUrl = rawDataObj.authorUrl;
+  this.body = rawDataObj.body;
 }
 
-var codeFellowsProject = new MyProject('Salmon Cookies', 'date', 'url');
+Article.prototype.toHtml = function() {
+  var $newArticle = $('article.template').clone();
+  $newArticle.removeClass('template');
+  if (!this.publishedOn) $newArticle.addClass('draft');
+  $newArticle.data('category', this.category);
+  $newArticle.attr('data-author', this.author);
+  $newArticle.find('.byline a').text(this.author);
+  $newArticle.find('.byline a').attr('href', this.authorUrl);
+  $newArticle.find('h2:first').text(this.title);
+  $newArticle.find('.article-body').html(this.body);
+  $newArticle.find('time[pubdate]').attr('datetime', this.publishedOn);
 
-// to be filled later
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+  $newArticle.append('<hr>');
+  return $newArticle;
+};
+
+rawData.sort(function(a,b) {
+  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+});
+
+rawData.forEach(function(articleObject) {
+  articles.push(new Article(articleObject));
+});
+
+articles.forEach(function(article) {
+  $('#articles').append(article.toHtml());
+});
